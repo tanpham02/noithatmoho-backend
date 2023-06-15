6e58d4ecf7fc345a3e934b30d0badc5bde957a57
Văn Tân <phamvantan1311@gmail.com>
2023-06-15 13:06:32 +0700

    update cors

--- src/main/java/com/api/noithatmoho_backend/Config/CorsConfig.java
+++ src/main/java/com/api/noithatmoho_backend/Config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
 	         @Override
 	         public void addCorsMappings(CorsRegistry registry) {
 	            registry.addMapping("/**")
-	               .allowedOrigins("http://localhost:3000")
+	               .allowedOrigins("https://moho.vercel.app")
 	               .allowedMethods("GET", "POST", "PUT", "DELETE")
 	               .allowCredentials(true);
 	         }
